54aea7ac3957a892599e04ad3f0a293a8a94c82b
Ivo Karaneshev <ivo.i.karaneshev@gmail.com>
2020-03-17 02:08:47 +0200

    Rename 'VacationManager' to 'Web'

---
+++ Web/Models/Projects/ProjectsEditViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Data.Entitiy;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web.Models.Projects
+{
+    public class ProjectsEditViewModel
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50, ErrorMessage = "Project cannot be more than 50 characters")]
+        public string ProjectName { get; set; }
+
+        [MaxLength(250, ErrorMessage = "Max characters for decsription are 250")]
+        public string Description { get; set; }
+
+        public ICollection<Team> WorkingTeams { get; set; }
+    }
+}
